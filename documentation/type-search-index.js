d0cf676443a6d5911508fac6ae7114c1d89a0ed2
DESKTOP-ADSICA7\dmitry <listofkin@gmail.com>
2022-09-27 01:35:24 +0700

    Edit

--- documentation/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.pmu1","l":"ExampleInstrumentedTest"},{"p":"com.example.pmu1","l":"ExampleUnitTest"},{"p":"com.example.pmu1","l":"MainActivity"},{"p":"com.example.pmu1","l":"MyMath"}]
