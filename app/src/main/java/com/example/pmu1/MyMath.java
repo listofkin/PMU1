14dd4e566377e13f4db33c6404741aceff3c47a0
DESKTOP-ADSICA7\dmitry <listofkin@gmail.com>
2022-09-27 01:25:44 +0700

    Adding JavaDoc

--- app/src/main/java/com/example/pmu1/MyMath.java
+++ app/src/main/java/com/example/pmu1/MyMath.java
@@ -1,11 +1,28 @@
 package com.example.pmu1;
-
+/**
+ * Class of mathematical functions <b>min</b> and <b>max</b>.
+ * @author Dmitry Shumikhin
+ * @version 1.1
+*/
 public class MyMath {
+    /**
+     * The function of finding the minimum among two integers
+     * @see MyMath#min(int, int)
+     * @param a - first int number
+     * @param b - second int number
+     * @return return minimum
+     */
     public int min(int a, int b){
         if(a < b) return a;
         return b;
     }
-
+    /**
+     * The function of finding the maximum among two integers
+     * @see MyMath#max(int, int)
+     * @param a - first int number
+     * @param b - second int number
+     * @return return maximum
+     */
     public int max(int a, int b){
         if(a > b) return a;
         return b;
--- app/src/test/java/com/example/pmu1/ExampleUnitTest.java
+++ app/src/test/java/com/example/pmu1/ExampleUnitTest.java
@@ -11,48 +11,72 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    /**
+     * Maximum Search Test
+     */
     @Test
     public void test1_max(){
         MyMath myMath = new MyMath();
         int actual = myMath.max(4,9);
         Assert.assertEquals(9, actual);
     }
+    /**
+     * Maximum Search Test
+     */
     @Test
     public void test2_max(){
         MyMath myMath = new MyMath();
         int actual = myMath.max(-1,2);
         Assert.assertEquals(2,actual);
     }
+    /**
+     * Maximum Search Test
+     */
     @Test
     public void test3_max(){
         MyMath myMath = new MyMath();
         int actual = myMath.max(11,9);
         Assert.assertEquals(11,actual);
     }
+    /**
+     * Maximum Search Test
+     */
     @Test
     public void test4_max(){
         MyMath myMath = new MyMath();
         int actual = myMath.max(5,5);
         Assert.assertEquals(5, actual);
     }
+    /**
+     * Minimum Search Test
+     */
     @Test
     public void test1_min(){
         MyMath mymath = new MyMath();
         int actual = mymath.min(-2,9);
         Assert.assertEquals(-2, actual);
     }
+    /**
+     * Minimum Search Test
+     */
     @Test
     public void test2_min(){
         MyMath mymath = new MyMath();
         int actual = mymath.min(-9,-16);
         Assert.assertEquals(-16, actual);
     }
+    /**
+     * Minimum Search Test
+     */
     @Test
     public void test3_min(){
         MyMath mymath = new MyMath();
         int actual = mymath.min(0,0);
         Assert.assertEquals(0, actual);
     }
+    /**
+     * Minimum Search Test
+     */
     @Test
     public void test4_min(){
         MyMath mymath = new MyMath();
